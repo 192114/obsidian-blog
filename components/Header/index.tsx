0e20063a0de60a7a8fb28ac2238d469033046eb1
192114 <shadowSun192114@gmail.com>
2024-01-29 18:03:58 +0800

    feat: 滚动到顶部

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,10 +7,14 @@ import ToggleTheme from '@/components/ToggleTheme'
 
 export default function Header() {
   const [hasScroll, setHasScroll] = useState(false)
+  const [showBackTop, setshowBackTop] = useState(false)
   useEffect(() => {
     const scrollListener = () => {
-      const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+      const scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop
       setHasScroll(scrollTop > 0)
+
+      setshowBackTop(scrollTop > 200)
     }
 
     window.addEventListener('scroll', scrollListener)
@@ -22,28 +26,44 @@ export default function Header() {
   }, [])
 
   return (
-    <header className={clsx('sticky top-0 z-50 w-full p-x-8 py-4 bg-background-80 backdrop-filter backdrop-blur-5px', hasScroll && 'shadow-md')}>
-      <nav className="flex-y-center h-full w-full gap-4 justify-end">
-        <Link href="/" className="flex-center">
-          a
-        </Link>
-        <Link href="/" className="flex-center">
-          b
-        </Link>
-        <Link href="/" className="flex-center">
-          c
-        </Link>
-        <Link
-          href="https://github.com/192114"
-          target="_blank"
-          className="flex-center"
-        >
-          <i className="i-lucide-github"></i>
-        </Link>
+    <>
+      <header
+        className={clsx(
+          'sticky top-0 z-50 w-full p-x-8 py-4 bg-background-80 backdrop-filter backdrop-blur-5px',
+          hasScroll && 'shadow-md'
+        )}
+      >
+        <nav className="flex-y-center h-full w-full gap-4 justify-end">
+          <Link href="/" className="flex-center">
+            a
+          </Link>
+          <Link href="/" className="flex-center">
+            b
+          </Link>
+          <Link href="/" className="flex-center">
+            c
+          </Link>
+          <Link
+            href="https://github.com/192114"
+            target="_blank"
+            className="flex-center"
+          >
+            <i className="i-lucide-github"></i>
+          </Link>
 
-        <ToggleTheme />
-      </nav>
-      
-    </header>
+          <ToggleTheme />
+        </nav>
+      </header>
+      <button
+        className={clsx(
+          'border-none bg-transparent text-text-weak text-lg fixed bottom-4 right-4 cursor-pointer hover:text-primary opacity-0 transition-all duration-300',
+          showBackTop && 'opacity-100', 
+          !showBackTop && 'pointer-events-none'
+        )}
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      >
+        <i className="i-lucide-arrow-up"></i>
+      </button>
+    </>
   )
 }
