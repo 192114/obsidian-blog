62cbf967e2cf329deb8d49b785596ceb0f8178f8
192114 <shadowSun192114@gmail.com>
2024-02-29 17:37:39 +0800

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,17 @@ import '@/styles/globals.css'
 export const metadata: Metadata = {
   title: 'shadow blog',
   description: '个人博客',
+  icons: [
+    { rel: 'icon', url: '/favicons/favicon.ico' },
+    {
+      rel: 'app-touch-icon',
+      url: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      url: '/favicons/site.webmanifest',
+    },
+  ],
 }
 
 export default function RootLayout({
